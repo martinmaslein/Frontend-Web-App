46e164b02cd64ce0bc75f1236e1bd4ef8d8c2b7f
Dylan Hughes <dylan_21174@hotmail.com>
2023-05-30 20:12:46 -0300

    Guitars Refactor

--- src/pages/guitars/acoustic-guitars.jsx
+++ src/pages/guitars/acoustic-guitars.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import CenterContent from "src/layouts/CenterContent.jsx";
+import ProductItem from 'src/components/ProductItem.jsx';
 
 function AcousticGuitar() {
 	const [products, setProducts] = useState([]);
@@ -26,17 +27,7 @@ function AcousticGuitar() {
 	
 			<div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
 				{filteredProducts.map((product) => (
-				<a key={product.id} href={"#"} className="group">
-				  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-					<img
-					  src={product.image_link}
-					  alt={product.name}
-					  className="h-full w-full object-cover object-center group-hover:opacity-75"
-					/>
-				  </div>
-				  <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-				  <p className="mt-1 text-lg font-medium text-gray-900">{"$"+product.price}</p>
-				</a>
+					<ProductItem key={product.id} product={product} />
 			  ))}
 			</div>
 			</CenterContent>

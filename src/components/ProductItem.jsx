6268ba1697293ad32a9a37b87a7d2816ce5c8117
Dylan Hughes <dylan_21174@hotmail.com>
2023-05-30 20:11:12 -0300

    Product Item component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function ProductItem({ product }) {
+  return (
+    <a href="#" className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+        <img
+          src={product.image_link}
+          alt={product.name}
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{"$" + product.price}</p>
+    </a>
+  );
+}
+
+export default ProductItem;

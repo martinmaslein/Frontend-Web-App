0a457133865b36f66492dcfc0b4c063693517c84
Martin Maslein <tincho_m98@hotmail.com>
2023-06-01 17:08:55 -0300

    listener addToCartButton

--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -5,6 +5,9 @@ import { useCookies } from 'react-cookie';
 function ProducOverview({ guitarDetails }) {
 
   const [cookies, setCookie] = useCookies(['cart']);
+  const addToCartButton = document.getElementById('addToCartButton');
+
+  addToCartButton.addEventListener('click', handleAddToCart);
 
   function handleAddToCart() {
     // Obtén el producto que se va a agregar al carrito
@@ -83,7 +86,7 @@ function ProducOverview({ guitarDetails }) {
           <div class="flex">
             <span class="title-font font-medium text-2xl text-gray-900">${guitarDetails.price}</span>
             <button class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Añadir al Carrito</button>
-            <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+            <button id='addToCartButton'class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
               <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                 <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
               </svg>

5810988dde05f451258fbcbf8ff8ebcb228b2924
Dylan Hughes <dylan_21174@hotmail.com>
2023-06-03 20:49:32 -0300

    Fix product display

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -20,15 +20,19 @@ function ProductCart({ product }) {
 
     return (
         <div className="grid grid-cols-3 items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-            <div className="flex">
-                <div>
+            <div className="flex flex-col justify-between ml-4 flex-grow">
+                <div className="flex items-center justify-center">
                     <img className="h-32 w-32" src={product.image_link} alt="" />
                 </div>
-                <div className="flex flex-col justify-between ml-4 flex-grow">
-                    <span className="font-bold text-md">{product.name}</span>
-                    <button onClick={() => removeItem()} className="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</button>
-                </div>
+                <span className="font-bold text-md text-center">{product.name}</span>
+                <button
+                    onClick={() => removeItem()}
+                    className="font-semibold hover:text-red-500 text-gray-500 text-xs"
+                >
+                    Remove
+                </button>
             </div>
+
             <div>
                 <QuantityInput product={product} />
             </div>

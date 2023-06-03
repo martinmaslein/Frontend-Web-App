988316f6b654ad468fc046a1a9478895cd3af6e2
Dylan Hughes <dylan_21174@hotmail.com>
2023-06-03 18:40:15 -0300

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,14 @@ function Footer() {
         <footer className="text-gray-600 body-font">
             <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                <img
-                    className="h-8 w-auto"
-                    src="https://res.cloudinary.com/dygwxsimn/image/upload/v1685048810/logo2_tendwb.png"
-                    alt=""
-                  />
+                    <img
+                        className="h-8 w-auto"
+                        src="https://res.cloudinary.com/dygwxsimn/image/upload/v1685048810/logo2_tendwb.png"
+                        alt=""
+                    />
                     <span className="ml-3 text-xl">Melody Marketplace</span>
                 </a>
-                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Copyright © Melody Marketplace, Inc. All Rights Reserved. —
-                    <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">Hughes Dylan | Martin Maslein</a>
+                <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">Copyright © Melody Marketplace, Inc. All Rights Reserved. — Hughes Dylan | Martin Maslein
                 </p>
                 <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                     <a className="text-gray-500">

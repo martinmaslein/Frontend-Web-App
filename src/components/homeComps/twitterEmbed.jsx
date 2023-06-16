da6c8d7b0109e5a76ac72125e45d0b19fbac13d8
Dylan Hughes <dylan_21174@hotmail.com>
2023-06-16 18:20:50 -0300

    Refactor twitterEmbed

---
+++ src/components/homeComps/twitterEmbed.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import Loading from "src/components/Loading.jsx";
+
+function TwitterEmbed() {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Tus artistas favoritos</h1>
+
+
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <div className="flex">
+
+                    <div className="w-1/2 mr-8">
+                        
+                        <TwitterTimelineEmbed
+                            onLoad={function noRefCheck() { }}
+                            options={{
+                                height: 500,
+                                width: 500,
+                            }}
+                            screenName="pogopedia"
+                            sourceType="profile"
+                            tweetLimit={10}
+                        // noHeader="false"
+                        />
+                    </div>
+                    <div className="w-1/2">
+                        
+                        <TwitterTimelineEmbed
+                            onLoad={function noRefCheck() { }}
+                            options={{
+                                height: 500,
+                                width: 500,
+                            }}
+                            screenName="crockpics"
+                            sourceType="profile"
+                            tweetLimit={10}
+                        // noHeader="false"
+                        />
+                    </div>
+                </div>
+            )}
+        </div>
+
+    );
+}
+
+export default TwitterEmbed;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import CenterContent from "src/layouts/CenterContent.jsx";
 import Jumbotron from "src/components/homeComps/Jumbotron.jsx";
 import Services from "src/components/homeComps/services.jsx";
-import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
-
+import TwitterEmbed from "src/components/homeComps/TwitterEmbed.jsx";
 
 function Home() {
 
@@ -11,21 +10,7 @@ function Home() {
 		<CenterContent>
 			<Jumbotron />
 			<Services />
-			<div className="centerContent">
-				<div className="selfCenter spaceBetween">
-					<TwitterTimelineEmbed
-						onLoad={function noRefCheck() { }}
-						options={{
-							height: 500,
-							width: 500,
-						}}
-						screenName="pogopedia"
-						sourceType="profile"
-						tweetLimit={10} 
-						noHeader= "false"
-					/>
-				</div>
-			</div>
+			<TwitterEmbed />
 		</CenterContent>
 
 	);

e086f52bc0e47b430d9250fe643a45d36721773a
Martin Maslein <tincho_m98@hotmail.com>
2023-06-28 17:20:51 -0300

    reorganized main components

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { initMercadoPago } from '@mercadopago/sdk-react'
+import { BrowserRouter} from 'react-router-dom';
 
 const PUBLIC_KEY = import.meta.env.VITE_REAT_APP_PUBLIC_KEY;
 initMercadoPago(PUBLIC_KEY, {
@@ -12,6 +13,8 @@ initMercadoPago(PUBLIC_KEY, {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )

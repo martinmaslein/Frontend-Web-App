f0f0c1033a125925b216d4dde43e474f1e3a1c2d
Martin Maslein <tincho_m98@hotmail.com>
2023-06-29 10:00:32 -0300

    several changes in useAuth

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,22 +1,25 @@
-import React, {useContext, useEffect} from "react";
-import {Cookies} from "react-cookie";
-import {useNavigate} from "react-router-dom";
+import React, { createContext, useContext, useEffect } from 'react';
+import { Cookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-import AuthContext from "../contexts/authContext";
+const AuthContext = createContext({
+    user: null,
+    setUser: () => { },
+    token: null,
+    setToken: () => { },
+    handleLogout: () => { }
+});
 
-export const useAuth = () => {
-    let navigate = useNavigate();
-
-    const [userData, setUserdata] = React.useState({signedIn: false, user: null});
-
-    const {setAuthData} = useContext(AuthContext);
+export const AuthProvider = ({ children }) => {
+    const [userData, setUserdata] = React.useState({ signedIn: false, user: null });
+    const navigate = useNavigate();
 
     useEffect(() => {
-        setAuthData(userData);
-    }, [userData.signedIn]);
+        setUserdata(userData);
+    }, [userData]);
 
-    function getAuthCookieExpiration()
-    {
+    function getAuthCookieExpiration() {
         let date = new Date();
         date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));  // 7 days
         return date;
@@ -24,48 +27,62 @@ export const useAuth = () => {
 
     function setAsLogged(user) {
         const cookie = new Cookies();
-        
-        cookie.set('is_auth', true, {path: '/', expires: getAuthCookieExpiration(), sameSite: 'lax', httpOnly: false});
+
+        cookie.set('is_auth', true, { path: '/', expires: getAuthCookieExpiration(), sameSite: 'lax', httpOnly: false });
         cookie.set('auth_token', user, { path: '/', sameSite: 'lax' });
-        
-        setUserdata({signedIn: true, user});
+
+        axios.get('http://127.0.0.1:8000/rest/user', {
+            headers: {
+                Authorization: `Bearer ${user}`
+            }
+        }).then(response => {
+            setUserdata({ signedIn: true, user: response.data.user});
+        });
+
         navigate('/');
     }
 
     function setLogout() {
         const cookie = new Cookies();
 
-        cookie.remove('is_auth', {path: '/', expires: getAuthCookieExpiration(), sameSite: 'lax', httpOnly: false});
-        cookie.remove('auth_token', {path: '/', expires: getAuthCookieExpiration(), sameSite: 'lax', httpOnly: false});
+        cookie.remove('is_auth', { path: '/', expires: getAuthCookieExpiration(), sameSite: 'lax', httpOnly: false });
+        cookie.remove('auth_token', { path: '/', expires: getAuthCookieExpiration(), sameSite: 'lax', httpOnly: false });
 
-        setUserdata({signedIn: false, user: null});
+        setUserdata({ signedIn: false, user: null });
         navigate('/login');
     }
 
-    function loginUserOnStartup()
-    {
+    function loginUserOnStartup() {
         const cookie = new Cookies();
-        if(cookie.get('is_auth')) {
+        if (cookie.get('is_auth')) {
 
             axios.get('http://127.0.0.1:8000/rest/user').then(response => {
-                setUserdata({signedIn: true, user: response.data.user});
+                setUserdata({ signedIn: true, user: response.data.user });
                 navigate('/');
             }).catch(error => {
-                setUserdata({signedIn: false, user: null});
+                setUserdata({ signedIn: false, user: null });
                 setLogout();
             });
 
         } else {
-            setUserdata({signedIn: false, user: null});
+            setUserdata({ signedIn: false, user: null });
             navigate('/login');
         }
     }
 
-    return {
-        userData,
-        setAsLogged,
-        setLogout,
-        loginUserOnStartup
-    }
-
+    return (
+        <AuthContext.Provider
+            value={{
+                userData,
+                setUserdata,
+                setAsLogged,
+                setLogout,
+                loginUserOnStartup
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
+
+export const useAuth = () => useContext(AuthContext);

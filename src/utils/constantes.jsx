332af8ec580400118bf0e366a1896f38d722d25c
Dylan Hughes <dylan_21174@hotmail.com>
2023-06-30 14:27:47 -0300

    Fix apiUrl

--- src/utils/constantes.jsx
+++ src/utils/constantes.jsx
@@ -1,2 +1,1 @@
-// export const apiUrl = "https://hughes-maslein-laravel-git-larave-ba45c8-hughes-maslein-laravel.vercel.app/rest/"
-export const apiUrl = 'http://127.0.0.1:8000/rest/'
+export const apiUrl = "https://hughes-maslein-laravel-qe45t662h-hughes-maslein-laravel.vercel.app//rest/"
